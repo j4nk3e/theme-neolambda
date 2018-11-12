7c23ad61f435309ace88e8aaabf80e097116e2dc
Chris Jones <chris.r.jones.1983@gmail.com>
2018-11-12 16:21:20 -0600

    logic error

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -42,7 +42,7 @@ function fish_prompt
     set -g location at
   else
     if [ "$TERM" = "xterm-256color-italic" -o "$TERM" = "tmux-256color" ]
-      set -g location "\e[3m ssh \e[23m"
+      set -g location (echo -e "\e[3m ssh \e[23m")
     else
       set -g location ssh
     end
